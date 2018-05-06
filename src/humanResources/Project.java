1b0ab764978e833a6bc7bc7f2042f1fa01a2dba5
Dmitriy <36268641+DrewTrue@users.noreply.github.com>
2018-05-06 20:52:54 +0400

    Update Project.java

--- src/humanResources/Project.java
+++ src/humanResources/Project.java
@@ -8,14 +8,10 @@ public class Project implements EmployeeGroup{
     private Node<Employee> head;
     private Node<Employee> tail;
 
-    private final static Node DEFAULT_HEAD = null;
-    private final static Node DEFAULT_TAIL = null;
     private final static int DEFAULT_SIZE = 0;
 
     public Project(String name){
         this.name = name;
-        this.head = DEFAULT_HEAD;
-        this.tail = DEFAULT_TAIL;
         this.size = DEFAULT_SIZE;
     }
 
